77f37642dee8b84cf2ed4066214715ca04f7caf1
Alan Iberra <alan@voyagerportal.com>
2021-05-02 21:46:58 -0300

    FRONT: Implement map

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,28 +1,43 @@
 import './App.css';
 import Post from './componets/Post'
 
-const post = {
-  likes: 20,
-  descriptions: 'Working on farm',
-  image: {
-    url: "/uploads/cropped_Tractor_546ae40f61.jpeg",
-    name: "cropped-Tractor.jpeg",
+const posts = [
+  {
+    likes: 20,
+    descriptions: 'Working on farm',
+    image: {
+      url: "/uploads/cropped_Tractor_546ae40f61.jpeg",
+      name: "cropped-Tractor.jpeg",
+    },
+    author: {
+      username: "AlanIberra",
+    }
   },
-  author: {
-    username: "AlanIberra",
+  {
+    likes: 82,
+    descriptions: 'Selfie on Coworking',
+    image: {
+      url: "/uploads/POI_3d4ee875ff.png",
+      name: "Selfie.png",
+    },
+    author: {
+      username: "AlanIberra",
+    }
   }
-};
+];
 
 function App() {
   return (
     <div className="App">
-    <Post
-        likes={post.likes}
-        descriptions={post.descriptions}
-        imageUrl={post.image && post.image.url}
-        imageName={post.image && post.image.name}
-        author={post.author && post.author.username}
-    />
+      {posts.map(post => (
+          <Post
+              likes={post.likes}
+              descriptions={post.descriptions}
+              imageUrl={post.image && post.image.url}
+              imageName={post.image && post.image.name}
+              author={post.author && post.author.username}
+          />
+      ))}
     </div>
   );
 }
